92025f432f13506dfb47c3da4f3ac2cddff3d26d
Mic Fox <michael.c.fox.ii@gmail.com>
2019-08-16 15:01:13 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :students, only: [:new, :show, :edit, :update, :create]
+  resources :school_classes, only: [:new, :show, :edit, :update, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+50.times.each do |num|
+    Student.create(first_name: "Name #{num}", last_name: "Last Name #{num}")
+end
+
+10.times.each do |num|
+    SchoolClass.create(title: "Class #{num}", room_number: "#{num}")
+end
